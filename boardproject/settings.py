05a983d22e748f7813f401bc0fd886ade4c1a298
tanaka takayuki <hawk.snow.1324@icloud.com>
2021-02-23 20:58:24 +0900

    初期設定完了

--- boardproject/settings.py
+++ boardproject/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'boardapp.apps.BoardappConfig'
 ]
 
 MIDDLEWARE = [
@@ -62,7 +63,7 @@ ROOT_URLCONF = 'boardproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
